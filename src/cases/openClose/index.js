fa380c44c0e769b9efc5f2ad122ed55b94cb9743
Voicu, Andrei-Florian <andrei-florian.voicu@haufe-lexware.com>
2020-03-09 21:31:58 +0200

    minor styling

--- src/cases/openClose/index.js
+++ src/cases/openClose/index.js
@@ -11,15 +11,14 @@ export default class OpenClose extends HTMLElement {
   connectedCallback() {
     this.innerHTML = /*html*/ `
       <style>
-        .button {
-          width: 100px;
-          height: 20px;
+        .oc-button {
+          width: 6rem;
           display: block;
           margin: 0 auto;
         }
       </style>
       <h1>Mode of ShadowRoot</h1>
-      <button class="button">click</button>
+      <button class="oc-button">click</button>
       <wc-open></wc-open>
       <wc-close></wc-close>
     `;
